6d9ad5e6fd34fb4b5d03edf8443f20190114765b
Pujan Poudel <pujanpoudelj7@gmail.com>
2024-07-02 18:49:26 +0545

    added sign in and sign up page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,71 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int i) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    var anim = AnimationController(
+      vsync: this,
+      value: 1,
+      duration: const Duration(milliseconds: 500),
+    );
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(child: Text("Welcome to the E-commerce App")),
+      extendBody: true,
+      body: Container(
+        child: Image.asset('assets/logo.png'),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: DotNavigationBar(
+          margin: EdgeInsets.only(left: 10, right: 10),
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          splashBorderRadius: 50,
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.home),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: Icon(Icons.search),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Profile
+            DotNavigationBarItem(
+              icon: Icon(Icons.person),
+              selectedColor: Color(0xff73544C),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+enum _SelectedTab { home, favorite, search, person }
